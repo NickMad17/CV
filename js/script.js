61052674e1d9e76ca8d0fc876cefb9fa4b29d482
Maady <104986153+NickMad17@users.noreply.github.com>
2023-05-26 15:45:32 +0300

    yolo

--- js/script.js
+++ js/script.js
@@ -55,7 +55,7 @@ ScrollReveal().reveal('.home__img, .about__content, .services__container, .portf
 /* -------------------- Type js ------------------------- */
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend develuper', 'Beatmaker', 'Microcontroller dev'],
+    strings: ['Frontend develuper', 'Beatmaker', 'C++ dev'],
     typeSpeed: 100,
     backSpeed:100,
     backDelay:1000,
